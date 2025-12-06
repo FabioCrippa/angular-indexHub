5d7348ba632425659ecac16eb5bda4e6cc1a284a
FabioCrippa <crippa.fabiocrippa@gmail.com>
2025-12-05 21:49:12 -0300

    updating dropdown

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  activeDetail: string = 'revistas'; // detalhe padrão ao abrir o dropdown
 
+  showDetail(detail: string) {
+    this.activeDetail = detail;
+  }
 }
