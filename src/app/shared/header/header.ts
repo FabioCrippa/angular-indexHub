a7a82ce2fac13d853639023e0affad64677c34ff
FabioCrippa <crippa.fabiocrippa@gmail.com>
2025-12-24 17:07:43 -0300

    add: hamburger menu

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   activeDetail: string = 'revistas'; // detalhe padrão ao abrir o dropdown
+  isMenuOpen: boolean = false;
+  isBuscaSubmenuOpen: boolean = false;
 
   showDetail(detail: string) {
     this.activeDetail = detail;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleBuscaSubmenuOpen() {
+    this.isBuscaSubmenuOpen = !this.isBuscaSubmenuOpen;
   }
 }
